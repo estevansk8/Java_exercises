1a32373ce722355c010d03ba564f12980d9cff29
Estevan Sousa <estevanmartins03@gmail.com>
2023-08-07 17:44:14 -0300

    :sparkles:feat: Ex1 complete with all tests corrects

--- src/main/java/br/edu/ifsp/list01/Ex01.java
+++ src/main/java/br/edu/ifsp/list01/Ex01.java
@@ -21,11 +21,35 @@ class Ex01 {
         //Leia o input
         //Crie uma variável do tipo deste arquivo. Exemplo: Ex02 ex = new Ex02();
         //Escreva o resultado da chamada do método compute() aqui
+        Scanner sc = new Scanner(System.in);
+        final int quantidade_livros = sc.nextInt();
+        final Ex01 ex01 = new Ex01();
+        System.out.println(ex01.compute(quantidade_livros));
     }
 
     String compute(int qtdLivros) {
-        String output = null;
-        //put your logic here
-        return output;
+        //faz o calculo de qual e o melhor
+        if (qtdLivros > 0){
+            double criterioA = calcular_criterioA(qtdLivros);
+            double criterioB = calcular_criterioB(qtdLivros);
+
+            if (criterioA<criterioB) return "Criterio A";
+            if (criterioA>criterioB) return "Criterio B";
+            if (criterioA==criterioB) return ("Indiferente");
+
+        }
+        return "Erro";
+    }
+
+    public static double calcular_criterioA(int quantidade_livros){
+        double valor = 7.5 + (quantidade_livros * 0.25);
+
+        return valor;
+    }
+
+    public static double calcular_criterioB(int quantidade_livros){
+        double valor = 2.5 + (quantidade_livros * 0.5);
+
+        return valor;
     }
 }
