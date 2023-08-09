e0ad886eb66c210a4285294d8d630f178cb7f94a
Estevan Sousa <estevanmartins03@gmail.com>
2023-08-09 09:13:28 -0300

    :sparkles:feat: Ex5 with all tests running

--- src/main/java/br/edu/ifsp/list01/Ex05.java
+++ src/main/java/br/edu/ifsp/list01/Ex05.java
@@ -1,5 +1,7 @@
 package br.edu.ifsp.list01;
 
+import java.util.Scanner;
+
 /*
     A avenida principal da cidade de Algoritmopolis possui limite de velocidade de L km/h.
     Se o motorista ultrapassar essa velocidade, é aplicado uma multa de R$ M, mais R$ A
@@ -24,13 +26,29 @@ public class Ex05 {
 
     public static void main(String[] args) {
         //Leia o input
+        Scanner sc = new Scanner(System.in);
+        final int limite = sc.nextInt();
+        final double multa = sc.nextDouble();
+        final double acrescimo = sc.nextDouble();
+        final int velocidade = sc.nextInt();
+
         //Crie uma variável do tipo deste arquivo. Exemplo: Ex02 ex = new Ex02();
+        final Ex05 ex05 = new Ex05();
+
         //Escreva o resultado da chamada do método compute() aqui
+        System.out.println(ex05.compute(limite, multa, acrescimo, velocidade));
     }
 
     String compute(int L, double m, double a, int v) {
-        String output =  null;
-        //put your logic here
-        return output;
+        int velocidade_excedida = v - L;
+        double valor_total = m + (a * velocidade_excedida);
+
+        String valor_total_com_dois_decimais = String.format("%.2f", valor_total);
+        String valor_formatado_com_ponto = valor_total_com_dois_decimais.replaceAll(",",".");
+
+
+        if (velocidade_excedida > 0) return valor_formatado_com_ponto;
+
+        return "0.00";
     }
 }
