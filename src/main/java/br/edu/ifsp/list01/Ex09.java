c6c83c1e9d74d68c5a6beff7d1826e3402649416
Estevan Sousa <estevanmartins03@gmail.com>
2023-08-14 16:36:52 -0300

    :poop:feat: Ex09 from list01 with tests failing and any wrong results

--- src/main/java/br/edu/ifsp/list01/Ex09.java
+++ src/main/java/br/edu/ifsp/list01/Ex09.java
@@ -1,5 +1,8 @@
 package br.edu.ifsp.list01;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /*
     Faça um programa que leia um conjunto de valores que correspondem as idades de pessoas de uma comunidade. Quando
     o valor fornecido for um número negativo, significa que não existem mais idades para serem lidas. Após a leitura,
@@ -20,13 +23,49 @@ public class Ex09 {
 
     public static void main(String[] args) {
         //Leia o input
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer> vetorIdade = new ArrayList<>();
+        while (true){
+            final int idade = sc.nextInt();
+            if (idade < 0)  break;
+            vetorIdade.add(idade);
+        }
+        int[] vetorIdadeArray = vetorIdade.stream().mapToInt(Integer::intValue).toArray();
         //Crie uma variável do tipo deste arquivo. Exemplo: Ex02 ex = new Ex02();
+        Ex09 ex09 = new Ex09();
         //Escreva o resultado da chamada do método compute() aqui
+        System.out.println(ex09.compute(vetorIdadeArray));
     }
 
-    String compute(int[] input) {
-        String output = null;
-        //put your logic here
-        return output;
+    String compute(int[] vetorIdadeArray) {
+        int somaDasIdades = 0;
+        int maioresDeIdade = 0;
+        int pessoasIdosas = 0;
+        int totalDePessoas = 0;
+
+        for (int idade: vetorIdadeArray) {
+            somaDasIdades += idade;
+            if (idade >= 18) maioresDeIdade += 1;
+            if (idade > 75) pessoasIdosas += 1;
+            totalDePessoas += 1;
+        }
+        double mediaDeIdade = somaDasIdades / totalDePessoas;
+
+        double porcentagemPessoasIdosas = pessoasIdosas * 100 / totalDePessoas;
+
+        //tratando como a média vai ser exibida
+        String media_com_dois_decimais = String.format("%.2f", mediaDeIdade);
+        String mediaDeIdade_formatada_com_ponto = media_com_dois_decimais.replaceAll(",",".");
+
+        //tratando como a quantidade dos maioresDeIdade vao ser exibidos
+        String stringmaioresDeIdade = Integer.toString(maioresDeIdade);
+
+        //tratando a forma como a porcentagem de pessoas idosas vai ser exibida
+        String porcentagemPessoasIdosasComDoisDecimais = String.format("%.2f", porcentagemPessoasIdosas);
+        String porcentagemPessoasIdosasComDoisDecimaisComPonto = porcentagemPessoasIdosasComDoisDecimais.replaceAll(",",".");
+
+        return mediaDeIdade_formatada_com_ponto + " " + stringmaioresDeIdade + " " + porcentagemPessoasIdosasComDoisDecimaisComPonto + "%";
+
     }
 }
--- src/test/java/br/edu/ifsp/list01/Ex09Test.java
+++ src/test/java/br/edu/ifsp/list01/Ex09Test.java
@@ -3,12 +3,12 @@ package br.edu.ifsp.list01;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Ex09Test {
-
-
 
     @ParameterizedTest
     @CsvSource(delimiter = '|', textBlock = """
